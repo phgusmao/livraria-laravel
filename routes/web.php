84a104bbadef29d453bc11480bbfb75bdb74db45
phgusmao <pedro.gusmao1998@hotmail.com>
2022-05-05 16:57:47 -0300

    iniciando projeto

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/menu', function () {
+    return view('menu');
+});
+
+Route::group(['prefix' => '/matematica/'], function () {
+    Route::get('soma', function () {
+        echo "Soma";
+    });
+    Route::get('subtracao', function () {
+        echo "Subtração";
+    });
+    Route::get('divisao', function () {
+        echo "Divisão";  
+    });
+    Route::get('multiplicacao', function () {
+        echo "multiplicacao";
+    });
+});
